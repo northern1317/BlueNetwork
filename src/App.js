7ef0841617cee2827b2ed57941534ccdbb2e0c1a
Aleh <aleh.baronin@alkemy.com>
2020-04-27 14:12:19 +0200

    Making a copy of the state in DialogReducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+import {Route} from 'react-router-dom';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  
+//anonymous functions 
+const ProfileComponent = () => <Profile />
+const DialogsComponent = () => <DialogsContainer />                       
+const NewsComponent = () => <News/>
+const MusicComponent = () => <Music/> 
+const SettingsComponent = () => <Settings/>
+
+  return ( 
+      <div className="app-area">
+        <div className="app-header">
+          <Header />
+        </div>
+
+        <div className="app-content">
+          <div className="app-navbar">
+          <Navbar />
+          </div> 
+
+          <div className="app-section">
+            <Route path='/profile' render={ProfileComponent} />
+            <Route path='/dialogs' render={DialogsComponent} />
+            <Route path='/news' render={NewsComponent} />
+            <Route path='/music' render ={MusicComponent} />
+            <Route path='/settings' render={SettingsComponent} />
+          </div>
+        </div>
+      </div>
   );
 }
 
